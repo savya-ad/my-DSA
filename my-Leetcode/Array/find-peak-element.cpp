ed27b51b2bbc92accaeaa2bb45dd5250967f8164
savya-ad <85820776+savya-ad@users.noreply.github.com>
2021-12-22 14:17:37 +0530

    Update find-peak-element.cpp

--- my-Leetcode/Array/find-peak-element.cpp
+++ my-Leetcode/Array/find-peak-element.cpp
@@ -1,5 +1,38 @@
 // Problem link:- https://leetcode.com/problems/find-peak-element/submissions/
 
+// O(log n) solution--
+class Solution {
+public:
+    int findPeakElement(vector<int>& arr) {
+        int n=arr.size();
+        if(n==1){
+            return 0;
+        }
+        if(n>1){
+            if(arr[0]>arr[1])   return 0;
+        }
+        int low=0;
+        int high=n-1;
+        while(low<high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]>arr[mid+1] and arr[mid]>arr[mid-1]){
+                low=mid;
+                break;
+            }
+            if(arr[mid+1]>arr[mid])
+                low=mid+1;
+            else
+                high= mid;
+            
+        }
+        return low;
+    }
+};
+
+
+
+
+// O(n) solution--
 class Solution {
 public:
     int findPeakElement(vector<int>& arr) {
