5f1b7313b31c6f64443839284ec222cc30505a25
savya-ad <85820776+savya-ad@users.noreply.github.com>
2022-01-10 19:37:00 +0530

    Create Delete Node in a Linked List.cpp

---
+++ my-Leetcode/linked-list/Delete Node in a Linked List.cpp
@@ -0,0 +1,21 @@
+// Problem link:- https://leetcode.com/problems/delete-node-in-a-linked-list/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        ListNode* curr=node;
+        curr->val=curr->next->val;
+        curr=curr->next;
+        node->next=curr->next;
+        delete curr;
+        
+    }
+};
